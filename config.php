773476b9f0b97c827c390b08a894d062de849575
Ralys <ralys@gmx.fr>
2013-09-25 13:49:03 +0200

    Update config.php

--- config.php
+++ config.php
@@ -5,7 +5,7 @@
 define('HOSTNAME', "localhost");
 define('DBNAME', "CadmusFramework");
 define('LOGIN', "root");
-define('PWD', 'Sokute57');
+define('PWD', '');
 
 /* TWIG config */
 define('LAYOUT_DIR', "layout");
